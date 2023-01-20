9ef927a6819850a300bc8a56a269761e27e3a854
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2023-01-20 09:27:06 +0100

    getElecmentById, getElementByClassName, querySelector, querySelectorAll

---
+++ Dom/index.js
@@ -0,0 +1,32 @@
+// getElementByClassName
+// const paragraphs = document.getElementsByClassName("p");
+
+// for (let i=0; i<paragraphs.length; i++){
+    // for each paragraph in the paragraphs that have a class of p, set their new class to red using (setAttribute)
+//     paragraphs[i].setAttribute('class', 'red');
+// }
+
+// getElementById
+// const body = document.getElementById("container");
+// console.log(body);
+
+// to create an element
+// const h2 = document.createElement("h2");
+// to add element to an element
+// h2.innerText = "I exist"
+// to add element to an parent element or container
+// body.appendChild(h2);
+
+// QUERY SELECTOR / QUERY SELECTORALL
+// selecting all paragraphs with a class of p ('.p')
+// const allParagraphs = document.querySelectorAll('.p');
+// console.log("all", allParagraphs);
+
+// selecting the body with an id of container ("#container")
+// const body2 = document.querySelector('#container');
+// console.log(body2);
+
+
+
+
+
