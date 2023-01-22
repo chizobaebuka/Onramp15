b12c7b656b3b66c7f1870a66f80fb07f49aa09b9
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2023-01-22 19:38:22 +0100

    solved the minimumbillcount algorithm using 2 different approaches

---
+++ Algorithms/minimumDollarBillCount.js
@@ -0,0 +1,44 @@
+// DESCRIPTION:
+// Find the minimum dollar bill's count to represent some value based on the availables bills.
+
+// You will receive the value and an array as input (the array contains the available bills).
+
+// For instance:
+
+// minimumBillCount(100, [10, 50, 20]) should return 2; (2x$50)
+
+// minimumBillCount(500, [100, 50, 20]) should return 5; (5x$100)
+
+// minimumBillCount(40, [1, 10, 20]) should return 2; (2x$20)
+
+// minimumBillCount(5, [1, 10, 20]) should return 5; (5x$1)
+
+// It is guaranteed that the value can always be divided into given bills, and there are no "tricky" edge cases.
+
+function minimumBill(value, numArr){
+    let bill = 0;
+    numArr.sort((a,b) => b-a).forEach((bill) => {
+        bill += parseInt(value / bill);
+        value %= bill;
+    })
+    return bill;
+}
+
+// OR 
+
+function minimumBillCount(value, bills) {
+    // sort bills in descending order
+    bills.sort((a, b) => b - a); 
+    // initializing the billCOunt to 0
+    let billCount = 0;
+    // iterating through the bills array to find the new billCount
+    for (let i = 0; i < bills.length; i++) {
+        // while the value given is >= bills[i]
+        while (value >= bills[i]) {
+            value -= bills[i];
+            billCount++;
+        }
+    }
+    return billCount;
+}
+
