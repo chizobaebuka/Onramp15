cfdbdae0cc2b19f27bf645df981afba4a0dd9b21
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2023-01-24 15:40:11 +0100

    added few functionalities to the todo list

--- Todo-List/todo.js
+++ Todo-List/todo.js
@@ -1,26 +1,94 @@
 let todos = [
     {id: 1, content: "sample todo", createdDate: new Date().toDateString() },
+    {id: 2, content: "I want to eat", createdDate: new Date().toDateString() },
+    {id: 3, content: "build a todo list app", createdDate: new Date().toDateString() },
 ];
 
 
 window.addEventListener('load', () => {
-    renderTodo(todos[0])
+    for (let todo of todos){
+        appendTodo(todos[0])
+    }
 });
 const container = document.querySelector('#container');
+const inputBox = document.querySelector('#input');
+const addButton = document.querySelector('#add');
 
-function renderTodo() {
+inputBox.addEventListener('keyup', () => {
+    let content = inputBox.value.trim();
+    if(!content){
+        alert('input Box is empty');
+        return;
+    }
+    if(Event.keyCode === 13){
+        addTodo(content);
+        inputBox.value = "";
+    }
+})
+
+addButton.addEventListener('onclick', () => {
+    let content = inputBox.value.trim();
+    if (!content){
+        alert('input box is empty');
+        return
+    }
+
+    addTodo(content);
+    inputBox.value = '';
+})
+
+function appendTodo(todo) {
     let div = document.createElement('div');
     // adding an attribute of class(todo) to the div which was created
     div.setAttribute('class', 'todo');
     // create p element
     let p = document.createElement('p');
-    // create an innerdiv
+    // setting the innerText of the p task to be set to the content of the todos object 
+    p.innerText = todo.content;
+    // create an innerDiv
     let innerDiv = document.createElement('div');
     // create sub paragraph
     let subParagraph = document.createElement('p');
+    // setting the innerText of the subParagraph task to be set to the date of the todos object 
+    subParagraph.innerText = todo.createdDate;
     // create img element
-    let img = document.createElement('img');
+    let deleteIcon = document.createElement('img');
     // setting attribute for the img element based off the id of the todos array(todo.id)
-    img.setAttribute('id', 'delete-' + todos.id);
+    deleteIcon.setAttribute('id', 'delete-' + todo.id);
+    // setting the class attribute for the img element
+    deleteIcon.setAttribute('class', 'clickable');
+    deleteIcon.setAttribute('src', '/Onramp15/Todo-List/delete.png');
 
+    deleteIcon.addEventListener('click', () => {
+        deleteTodo(todo.id)
+    })
+
+    innerDiv.appendChild(subParagraph);
+    innerDiv.appendChild(deleteIcon);
+
+    div.appendChild(p);
+    div.appendChild(innerDiv);
+
+    container.appendChild(div);
 }
+
+function addTodo(content){
+    const newTodo = {
+        id : todos[todos.length - 1].id + 1,
+        content: content,
+        createdDate: new Date.toDateString()
+    }
+    todos.push(newTodo);
+    appendTodo(newTodo);
+}
+
+function deleteTodo(id){
+    todos = todos.filter((todo) => todo.id !== id);
+    container.innerHTML = null;
+    for (let todo of todos){
+        appendTodo(todo);
+    };
+}
+
+
+
