bef88873c1706f1d7e86a16c39cb518b3bb4a84a
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2023-01-27 13:19:56 +0100

    algorithm challenge solved

---
+++ Algorithms/search.js
@@ -0,0 +1,5 @@
+// Given a sorted array of integers, write a function called search that accepts a value and returns the index where the value passed to the function is located. If the value is not found return -1. For example, search([1,2,3,4,5,6], 4)
+
+function search(arr, n){
+    return arr.indexOf(n);
+}
