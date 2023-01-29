a019123f08ba3e1e8427348b6e70b07dd78a4441
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2023-01-29 23:10:18 +0100

    added the correct solution to the algorithm challenge

--- Algorithms/removeNumber.js
+++ Algorithms/removeNumber.js
@@ -4,3 +4,18 @@ function removeNumbers(str){
     let newStr = str.split('').map((item) => item.replace(/[0-9]/g, '')).join('');
     return newStr;
 }
+
+///or
+
+function removeNumbers(str) {
+    let removedNumbers = [];
+    let newStr = "";
+    for (let i = 0; i < str.length; i++) {
+        if (isNaN(str[i])) {
+            newStr += str[i];
+        } else {
+            removedNumbers.push(parseInt(str[i]));
+        }
+    }
+    return { removedNumbers, newStr };
+}
