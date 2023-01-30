58928c27e0cc0359d15c410a3e40a345f15020f7
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2023-01-30 23:36:41 +0100

    corrected the function parameters

--- Algorithms/reverseMultiply.js
+++ Algorithms/reverseMultiply.js
@@ -1,6 +1,6 @@
 // Given an array of numbers, multiply each number in the array by the number of elements in the array in descending order
 
-function reverseMultiply(arr, n){
+function reverseMultiply(arr){
     let n = arr.length;
     let newArr = arr.sort((a,b) => b - a).map((item) => item * n);
     return newArr;
