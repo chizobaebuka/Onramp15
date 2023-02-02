e28f2554d03b2b91444d9170cb1a0681ccdbc657
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2023-02-02 11:10:13 +0100

    algorthm challenge solved

---
+++ Algorithms/convertToDecimal.js
@@ -0,0 +1,8 @@
+function convertToDecimal(binary){
+    let result = 0;
+    
+    for(let i= 0; i<binary.length; i++){
+        result += binary[i] * Math.pow(2, binary.length - i - 1)
+    }
+    return result;
+}
