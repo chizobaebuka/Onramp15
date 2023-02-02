f91448d4521e6a6ef1d2ffba4f93cf2c8bae294b
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2023-02-02 16:58:24 +0100

    algorthm challenge solved

---
+++ Algorithms/quickSort.js
@@ -0,0 +1,19 @@
+function quickSort(arr){
+    if(arr.length <= 1){
+        return arr;
+    }
+
+    let pivot = arr[arr.length -1];
+    let left = [];
+    let right = [];
+
+    for(let i = 0; i<arr.length -1 ; i++){
+        let char = arr[i];
+        if(char < pivot){
+            left.push(char);
+        }else{
+            right.push(char);
+        }
+    }
+    return [...quickSort(left), pivot, ...quickSort(right)]
+}
