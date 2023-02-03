797595cfef8bd4e21de39948eafa1a806ed51528
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2023-02-03 23:24:33 +0100

    algorthm challenge solved

--- Algorithms/counttruthy.js
+++ Algorithms/counttruthy.js
@@ -10,20 +10,3 @@ function countTruthy (arr){
     return count;
 }
 
-// Return the length of the longest word in the provided sentence.
-
-// (“The quick brown fox jumped over the lazy dog”)
-
-// Your response should be a number
-
-function longestWord(sentence){
-    let count = 0;
-    words = sentence.split('');
-
-    words.forEach((word) => {
-        if (word.length > longest) {
-            return count = word.length;
-        }
-    });
-    return count
-}
