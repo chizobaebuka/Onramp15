e7b0117b6da44d512fbe6b7a35b3be7e595810f9
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2023-02-03 23:25:20 +0100

    algorthm challenge solved

---
+++ Algorithms/longestWord.js
@@ -0,0 +1,17 @@
+// Return the length of the longest word in the provided sentence.
+
+// (“The quick brown fox jumped over the lazy dog”)
+
+// Your response should be a number
+
+function longestWord(sentence){
+    let count = 0;
+    words = sentence.split('');
+
+    words.forEach((word) => {
+        if (word.length > count) {
+            return count = word.length;
+        }
+    });
+    return count
+}
