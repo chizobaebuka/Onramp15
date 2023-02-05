92274f0ecff13d60f79b56a4e24a32c97d21dd28
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2023-02-05 17:52:14 +0100

    algorthm challenge solved using the forEach method

--- Algorithms/isDivisibleByN.js
+++ Algorithms/isDivisibleByN.js
@@ -22,3 +22,18 @@ function isDivisibleByN(arr, n){
 function isDivisibleByN(arr, n){
     return arr.filter((item) => item % n === 0);
 }
+
+
+// Implement a function which multiplies two numbers. 
+// Complete the function which takes two arguments and returns all numbers which are divisible by the given divisor. FIrst argument is an array of numbers and the second is the divisor.
+
+function divisibleByN(arr, n){
+    result = [];
+
+    arr.forEach((item) => {
+        if(item % n === 0){
+            result.push(item);
+        }
+    })
+    return result;
+}
