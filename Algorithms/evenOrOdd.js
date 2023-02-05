c94bbf109f6a762b0203c3566da871df8030bfbf
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2023-02-05 20:40:24 +0100

    algorithm challenge solved

---
+++ Algorithms/evenOrOdd.js
@@ -0,0 +1,12 @@
+// Create a function that takes an integer as an argument and returns "Even" for even numbers and "Odd" for odd numbers.
+// Example
+// (even_or_odd(8)) -> Even 
+// (even_or_odd(7)) -> Odd 
+
+function even_or_odd (int){
+    if(int % 2 === 0){
+        return "Even"
+    }else{
+        return "False";
+    }
+}
