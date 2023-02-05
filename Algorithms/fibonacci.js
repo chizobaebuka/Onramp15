0cffc32bf157d62dbc2a79678ce487520e764805
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2023-02-05 17:49:11 +0100

    solved algorthm challenge

---
+++ Algorithms/fibonacci.js
@@ -0,0 +1,17 @@
+// Fibonacci number (Fibonacci sequence), named after mathematician Fibonacci, is a sequence of numbers that looks like this:
+
+// 0, 1, 1, 2, 3, 5, 8, 13,...
+// You get first two starting numbers, 0 and 1, and the next number in the sequence is always the sum of the previous two numbers. Fibonacci introduced it in 1202, in his book Liber Abaci.
+
+// So what will be your task? You should write a function fib, that takes one parameter steps, and returns a number from the Fibonacci sequence, based on the parameter steps, which determines the position in Fibonacci number.
+
+// For example fib(0) returns 0, fib(4) returns 3, fib(15) returns 610.
+
+
+function fib(steps){
+    let fibNum = [0, 1];
+    for (i=0; i< steps; i++){
+        fibNum.push(fibNum[i] + fibNum[i+1])
+    }
+    return fibNum[i];
+}
