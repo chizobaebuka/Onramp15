7cd11314c5bd8534f5b83dbdba562c32be0caa1c
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2023-02-07 15:18:53 +0100

    algorthm challenges solved

---
+++ Algorithms/firstMultiples.js
@@ -0,0 +1,16 @@
+// Write a function to return the number of multiples of a number, that is divisible by the divisor
+// function firstMultiplesOf(noOfElements, number, divisor)
+
+function firstMultiplesOf(noOfMultiples, number, divisor){
+    let multiples = [];
+
+    if(number % divisor === 0){
+        divisor = 1;
+    }
+
+    for(let i =0; i <= noOfMultiples; i++){
+        let multiple = number * divisor * i;
+        multiples.push(multiple);
+    }
+    return multiples;
+}
