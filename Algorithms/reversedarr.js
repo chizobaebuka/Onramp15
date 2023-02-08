34bc8f547229652c8da0bfbe6cfe454faa6dc0da
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2023-02-08 23:07:28 +0100

    algorithm challenge solved

--- Algorithms/reversedarr.js
+++ Algorithms/reversedarr.js
@@ -1,5 +1,5 @@
 // Convert number to reversed array of digits. GIven a random non-negative number, you have to return the digits of this number within an array in reverse order.
-Example
+// Example
 // 348597--> [7, 9, 5, 8, 4, 3]
 
 function reversedDigits(num){
